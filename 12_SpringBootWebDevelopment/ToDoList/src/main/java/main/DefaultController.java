81edce7c861ce080441f2e404b79c8a6b307ad5b
Kazarnikov Alexander <kazarnikv-sanja@mail.ru>
2020-11-20 17:02:41 +0300

    Spring test Date

---
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String string() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("d.M.yyyy ~ HH:mm");
+        return simpleDateFormat.format(new Date());
+    }
+}
+

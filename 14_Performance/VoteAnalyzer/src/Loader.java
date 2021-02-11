caa3a37608fc7e8cd68b078ad9d6d3215eb02675
Kazarnikov Alexander <kazarnikv-sanja@mail.ru>
2021-02-11 01:09:28 +0000

    multiple insertion and indexing

--- 14_Performance/VoteAnalyzer/src/Loader.java
+++ 14_Performance/VoteAnalyzer/src/Loader.java
@@ -1,14 +1,18 @@
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import static java.lang.Runtime.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static java.lang.Runtime.getRuntime;
 
 public class Loader {
     public static void main(String[] args) throws Exception {
         int sizeMB = 1048576;
         long startTime = System.currentTimeMillis();
 
-        String fileName = "res/data-18M.xml";
+        String fileName = "res/data-1572M.xml";
+        countingVoters(fileName);
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser = factory.newSAXParser();
         XMLHandler handler = new XMLHandler();
@@ -17,12 +21,30 @@ public class Loader {
         parser.parse(fileName, handler);
         long e = (getRuntime().totalMemory() - getRuntime().freeMemory() - InitMemorySize) / sizeMB;
 
-        handler.printDuplicatedVoter();
+        DBConnection.printVoterCounts();
+
+        long startQueryTime = System.currentTimeMillis();
+        DBConnection.customSelect();
+        long endQueryTime = System.currentTimeMillis() - startQueryTime;
+
+        DBConnection.getConnection().close();
 
         long end = System.currentTimeMillis() - startTime;
         System.out.println("Lead Time: " + end + " ms.");
+        System.out.println("Query duration: " + endQueryTime + " ms.");
         System.out.println("Free of memory " + getRuntime().freeMemory() / sizeMB + " MB");
         System.out.println("Maximum memory " + getRuntime().maxMemory() / sizeMB + " MB");
         System.out.println("Memory used: " + e + " MB");
+    }
+
+    private static void countingVoters(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            System.out.printf("\u001B[34m" + "In file %,d voters." + "\u001B[0m\n", reader
+                    .lines()
+                    .filter(e -> e.contains("<voter name="))
+                    .count());
+        } catch (Exception e) {
+            System.err.println(e.getLocalizedMessage());
+        }
     }
 }
--- 14_Performance/VoteAnalyzer/src/XMLHandler.java
+++ 14_Performance/VoteAnalyzer/src/XMLHandler.java
@@ -1,42 +1,38 @@
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.HashMap;
+import java.sql.SQLException;
 
 public class XMLHandler extends DefaultHandler {
 
-    private Voter voter;
-    private final HashMap<Voter, Byte> voterCounts;
-
-    public XMLHandler() {
-        voterCounts = new HashMap<>();
+    @Override
+    public void startDocument() {
+        System.out.println("Start parse XML... ");
     }
 
     @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) {
-        if (qName.equals("voter") && voter == null) {
-            String birthDay = attributes.getValue("birthDay");
-            String name = attributes.getValue("name");
-            voter = new Voter(name, birthDay);
-        } else if (qName.equals("visit") && voter != null) {
-            byte count = voterCounts.getOrDefault(voter, (byte) 0);
-            voterCounts.put(voter, (byte) (count + 1));
-        }
-    }
-
-    @Override
-    public void endElement(String uri, String localName, String qName) {
-        if (qName.equals("voter")) {
-            voter = null;
-        }
-    }
-
-    public void printDuplicatedVoter() {
-        for (Voter voter : voterCounts.keySet()) {
-            byte count = voterCounts.get(voter);
-            if (count > 1) {
-                System.out.println(voter.toString() + " - " + count);
+    public void startElement(String uri,
+                             String localName,
+                             String qName,
+                             Attributes attributes) {
+        try {
+            if (qName.equals("voter")) {
+                String birthDay = attributes.getValue("birthDay");
+                String name = attributes.getValue("name");
+                DBConnection.countVoter(name, birthDay);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void endDocument() {
+        try {
+            DBConnection.executeMultiInsert();
+            System.out.println("Stop parse XML...");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
